ac012b44eb5104420d08098486aa8a17ac9e38fe
MiadongDeersfeet <ani.ana.yungi@gmail.com>
2025-08-21 11:15:19 +0900

    hometoacademy

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -1,5 +1,13 @@
 package com.kh.network.run;
 
 public class TestClass {
-
+	
+	// 이번엔 진짜 확정 지을 거임
+	// Home Sweet Home ~~~
+	// 집 좋다 좋아 ~~~ 이게 집이지~~~
+	// 나는 왜 안올라가지? 무ㅡㅓ지 ?? 왜이러지?????
+	
+	public void hi() {
+		
+	}
 }
